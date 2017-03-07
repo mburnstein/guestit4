62fe2ac9902fc47be77975b0cac531cfb7637141
Michael Burnstein <michael@burnstein.com>
2017-03-07 07:55:41 +0000

    Stuck with controller problem

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -2,4 +2,20 @@ class GuestsController < ApplicationController
   def index
     @guests=Guest.all
   end
+
+  def new
+    @guest = Guest.new
+  end
+
+  def create
+    Guest.create(guest_params)
+  end
+
+ 
+  private
+
+  def guest_params
+    params.require(:guest).permit(:first_name, :last_name)
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'guests#index'
+  resources :guests
 end
